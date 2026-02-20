ddff9f2883d0ceb62300520b16f335adf66fa970
Emre Taşar <emre.taser.07@gmail.com>
2026-02-20 14:01:49 +0300

    Finished engine phases.

--- src/engine/core/cre_engine.c
+++ src/engine/core/cre_engine.c
@@ -7,13 +7,52 @@
 #include "engine/ecs/cre_entityManager.h"
 #include "engine/systems/physics/cre_physicsSystem.h"
 #include "engine/systems/render/cre_rendererCore.h"
+#include "engine/ecs/cre_entitySystem.h"
+#include "engine/systems/animation/cre_animationSystem.h"
 #include "engine/loaders/cre_assetManager.h"
 #include "engine/platform/cre_viewport.h"
 #include "engine/systems/camera/cre_cameraSystem.h"
 #include "cre_commandBus.h"
-#include "engine/core/cre_enginePhases.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+// ENGINE PHASES
+static void EnginePhase0_PlatformSync(void) {
+    Viewport_Update();
+    if (Viewport_wasResized()) {
+        ViewportSize vp = Viewport_Get();
+
+        cameraSystem_UpdateViewportCache(vp);
+        rendererCore_RecreateCanvas((int32_t)vp.width,(int32_t)vp.height);
+        Log(LOG_LVL_INFO,"[ENGINE] Resolution updated to %0.fx%0.f",vp.width,vp.height);
+    }
+}
+
+static void EnginePhase1_InputAndLogic(EntityRegistry* restrict reg,CommandBus* bus,float dt) {
+    Input_Poll(); // Empty right now.
+
+    // SceneManager handles input right now due to raylib.
+    SceneManager_Update(reg,bus,dt); 
+}
+
+static void EnginePhase2_Simulation(EntityRegistry* restrict  reg,CommandBus* bus,float dt) {
+    // AI and Particle systems are not implemented right now.
+    EntitySystem_Update(reg,bus);
+    PhysicsSystem_Update(reg,bus,dt);
+    AnimationSystem_Update(reg,dt);
+}
+
+static void EnginePhase3_RenderState(EntityRegistry* restrict reg, CommandBus* bus, float dt) {
+    cameraSystem_Update(reg,bus,dt);
+
+    rendererCore_BeginFrame();
+    SceneManager_Draw(reg,bus);
+    rendererCore_EndFrame();
+}
+static void EnginePhase4_Cleanup(EntityRegistry* restrict reg, CommandBus* bus) {
+    CommandIterator iter = CommandBus_GetIterator(bus);
+    CommandBus_Flush(bus,&iter);
+}
 
 void Engine_Init(EntityRegistry* reg, CommandBus* bus,const char* title, const char* configFileName) {
     Logger_Init();
@@ -49,8 +88,7 @@ void Engine_Run(EntityRegistry* reg, CommandBus* bus,float dt) {
         EnginePhase1_InputAndLogic(reg,bus,dt);
         EnginePhase2_Simulation(reg,bus,dt);
         EnginePhase3_RenderState(reg,bus,dt);
-        CommandIterator iter = CommandBus_GetIterator(bus);
-        CommandBus_Flush(bus,&iter);
+        EnginePhase4_Cleanup(reg,bus);
     }
 }
 void Engine_Shutdown(EntityRegistry* reg, CommandBus* bus) {
