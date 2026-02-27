b532363638eb83cdbd00ff13bca53024aebee9d5
Emre Taşar <emre.taser.07@gmail.com>
2026-02-27 14:49:22 +0300

    CommandBus on heap

--- src/main.c
+++ src/main.c
@@ -11,19 +11,23 @@
 
 int main(void) {
     // Using alloc because EntityRegistry is rather big.
-    size_t allocSize = (sizeof(EntityRegistry) + 63) & ~63;
-    EntityRegistry* reg = aligned_alloc(64,allocSize);
+    size_t regAllocSize = (sizeof(EntityRegistry) + 63) & ~63;
+    EntityRegistry* reg = aligned_alloc(64,regAllocSize);
     if (!reg) return -1;
 
-    CommandBus bus;
+    size_t busAllocSize = (sizeof(CommandBus) + 63) & ~63;
+    CommandBus* bus = aligned_alloc(64,busAllocSize);
+    if (!bus) return -1;
+    
     float dt = 0.0166f;
 
-    Engine_Init(reg, &bus, GAME_TITLE, dirCONFIG);
+    Engine_Init(reg, bus, GAME_TITLE, dirCONFIG);
     SceneManager_Init(Game_GetScene);
     SceneManager_ChangeScene(GAME_STATE_PLAYING);
-    Engine_Run(reg, &bus, dt);
-    Engine_Shutdown(reg, &bus);
+    Engine_Run(reg, bus, dt);
+    Engine_Shutdown(reg, bus);
 
+    free(bus);
     free(reg);
     return 0;
 }
